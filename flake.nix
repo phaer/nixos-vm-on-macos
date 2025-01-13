5e6bee2ee38a971097eaea280e0377beb41b18d2
phaer <hello@phaer.org>
2025-01-13 21:38:07 +0100

    switch back to nixos-unstable
    since https://github.com/NixOS/nixpkgs/pull/372672 has hit that channel.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   description = "One-(digit-)click NixOS VM on macOS";
 
   inputs = {
-    # TODO https://github.com/NixOS/nixpkgs/pull/372672
-    # nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    nixpkgs.url = "github:phaer/nixpkgs?ref=vfkit-update";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
